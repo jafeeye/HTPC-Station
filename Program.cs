ad82b7bc3e3ff449684f215be16bea3825ddf67d
jeyrtt <60909823+jafeeye@users.noreply.github.com>
2020-03-24 18:18:57 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace es_bluetooth
+{
+    static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Process.Start("rundll32.exe", "shell32.dll,Control_RunDLL Bthprops.cpl");
+            Application.Exit();
+        }
+    }
+}
