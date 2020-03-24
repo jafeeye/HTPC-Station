86aec9633272e71ea969982488f3ca4767c593b1
jeyrtt <60909823+jafeeye@users.noreply.github.com>
2020-03-24 18:20:32 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace es_close
+{
+    static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Process[] MyProcess = Process.GetProcessesByName("emulationstation");
+            if (MyProcess.Length > 0)
+                MyProcess[0].Kill(); //關閉執行中的程式
+            Application.Exit();
+        }
+    }
+}
