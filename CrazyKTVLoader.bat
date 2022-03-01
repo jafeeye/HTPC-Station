f15ee55d83a0b4096ab90bd6810323a7259fe2da
jeyrtt <60909823+jafeeye@users.noreply.github.com>
2022-03-01 16:45:22 +0800

    Add files via upload

--- CrazyKTVLoader.bat
+++ CrazyKTVLoader.bat
@@ -1,5 +1,9 @@
 @echo off
-chdir /d E:\HTPC\CrazyKTV
-start "" "E:\HTPC\CrazyKTV\CrazyKTV.exe"
+REM chdir /d E:\HTPC\Apps\CrazyKTV
+REM start "" "E:\HTPC\Apps\CrazyKTV\CrazyKTV.exe"
+REM 切換到當前目錄
+CD /D "%~dp0"
+REM 執行程式
+start "" CrazyKTV.exe
 
 REM 再用 BAT TO EXE Converter 將 BAT 轉成 EXE。
